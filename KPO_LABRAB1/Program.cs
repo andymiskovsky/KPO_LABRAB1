b0018ee467cf39537636217a8c4a2bf4e7a163e7
andymiskovsky <nuclearhiems@hotmail.com>
2017-10-13 20:56:47 +0300

    LR1 default ver

---
+++ KPO_LABRAB1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPO_LABRAB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kak je ya lublu git, yeah");
+            Console.ReadKey();
+        }
+    }
+}
